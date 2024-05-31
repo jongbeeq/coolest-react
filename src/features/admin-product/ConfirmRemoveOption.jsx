9510e10b0e213b28c0f0fb89cdc65b1dda4ef0a1
jongbeeq <titivutewhangsukij@gmail.com>
2024-06-01 01:43:44 +0700

    CreateProduct:UpdateConfirmRemoveComponent

--- src/features/admin-product/ConfirmRemoveOption.jsx
+++ src/features/admin-product/ConfirmRemoveOption.jsx
@@ -15,12 +15,12 @@ export default function ConfirmRemoveOption() {
     return (
         <div className="w-full h-full flex justify-center items-center">
             <div className="absolute w-full h-full bg-neutral-100 opacity-70"></div>
-            <div className="absolute px-[5%] h-[60%] flex flex-col bg-neutral-cross drop-shadow-md">
+            <div className="absolute px-[5%] py-[2%] h-[60%] min-h-[40px] flex flex-col bg-neutral-cross drop-shadow-md">
                 <TextDetail className={'flex-grow text-nowrap flex justify-center gap-[3%] items-center text-[max(1vw,10px)]'}>
                     <InfoIcon className={'text-[max(1.5vw,12px)] text-neutral-sub-base'} />
                     Are you sure to delete? Option data will be lost.
                 </TextDetail>
-                <div className="w-full pb-[2%] flex items-center gap-[10px] justify-end">
+                <div className="w-full flex items-center gap-[10px] justify-end">
                     <div onClick={handleClickDelete}>
                         <TextDetail className={'font-bold cursor-pointer hover:text-neutral-sub-base text-[max(0.4vw,8px)]'}>Delete</TextDetail>
                     </div>
